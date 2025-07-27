eb713f4d2873eddb900e77c2ee85a0b168f56945
junpreetham <arjunpreetham49@gmail.com>
2025-07-27 18:05:33 +0800

    Updates

--- docs-site/python/complete.py
+++ docs-site/python/complete.py
@@ -15,20 +15,20 @@ def callback_fn(data):
 async def on_connected():
     print("Connected to Relay!")
 
-    sent = await client.publish("chat.room1", {
+    sent = await client.publish("$topic", {
         "user_name": "John Doe",
         "message": "How's it going fam?"
     })
 
     print(f"Message sent => {sent}")
 
-    unsubscribed = await client.off("chat.room1")
-    print(f"Unsubscribed from chat.room1 => {unsubscribed}")
+    unsubscribed = await client.off("$topic")
+    print(f"Unsubscribed from $topic => {unsubscribed}")
 
     await client.close()
 
 async def main():
-    await client.on("chat.room1", callback_fn)
+    await client.on("$topic", callback_fn)
     await client.on(Realtime.CONNECTED, on_connected)
 
     await client.connect() 
