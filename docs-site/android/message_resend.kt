5016d4f34a23cb78e4221a099cb226de5a4d9143
junpreetham <arjunpreetham49@gmail.com>
2025-07-29 22:50:14 +0800

    message_resend

--- docs-site/android/message_resend.kt
+++ docs-site/android/message_resend.kt
@@ -4,7 +4,7 @@ client.on(Realtime.MESSAGE_RESEND) { payload ->
     Log.d("Message", payload)
 
     /*
-    $payload is an array of,
-    [{"topic": <String, topic which the message was sent to>, "message": <Your Message>, "resent": <Boolean, if it was resent or not>}, ....]
+    $payload is an array (MutableList of HashMap<String, Any>) of,
+    val payload = mapOf("data" to [{"topic": <String, topic which the message was sent to>, "message": <Your Message>, "resent": <Boolean, if it was resent or not>}, ....])
     */
 }
