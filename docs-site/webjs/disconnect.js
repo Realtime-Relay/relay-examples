62539beed0160ca0a4caa14197bf0fb9d7df3cb4
junpreetham <arjunpreetham49@gmail.com>
2025-11-06 17:22:21 +0530

    Changed disconnect code

--- docs-site/webjs/disconnect.js
+++ docs-site/webjs/disconnect.js
@@ -1,15 +1,1 @@
-import { Realtime } from "relayx-webjs"
-
-const client = new Realtime({
-  api_key: '$api_key',
-  secret: '$secret'
-})
-
-client.init()
-
-// ... Application Code
-client.close()
-
-// ... Other Application Code
-
 client.connect()
