e6a9b17a48fd2b694f5369ee94d7f3c4a8690970
junpreetham <arjunpreetham49@gmail.com>
2025-11-07 12:38:10 +0530

    Sub message structure

--- docs-site/ios/subscribe.swift
+++ docs-site/ios/subscribe.swift
@@ -9,11 +9,3 @@ let listener = MyMessageListener()
 
 // Returns a boolean
 let subscribed =  try await client.on(topic: "chat.room1", listener: listener)
-
-// -------------------------------------------------
-// $message is a dict
-[
-    "id": "<MESSAGE ID>",
-    "topic": "<TOPIC MATCHING TOPIC / WILDCARD TOPIC>",
-    "data": <Actual message as string, number or json>
-]
